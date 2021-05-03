8c204e72fa9774ab5bcb5d5a494d43af1f3d5613
Mads Harby <71691732+madshby@users.noreply.github.com>
2021-05-03 11:00:18 +0200

    Added LoginDAO and validate method + implemented User Class

--- src/BE/User.java
+++ src/BE/User.java
@@ -1,4 +1,34 @@
 package BE;
 
 public class User {
+
+    private final int id;
+    private String fullName;
+    private boolean isAdmin;
+
+    public User(int id, String fullName, Boolean isAdmin){
+        this.id = id;
+        this.fullName = fullName;
+        this.isAdmin = isAdmin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
 }
---
+++ src/DAL/DAO/LoginDAO.java
@@ -0,0 +1,44 @@
+package DAL.DAO;
+
+import BE.User;
+import DAL.DBConnection.JDBCConnectionPool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginDAO {
+
+    private int id;
+    private String fullName;
+    private boolean isAdmin;
+
+
+    private final JDBCConnectionPool connectionPool;
+
+    /**
+     * Constuctor for the JDBCConnectionPool.
+     * @param connectionPool
+     */
+    public LoginDAO(JDBCConnectionPool connectionPool) {
+        this.connectionPool = connectionPool;
+    }
+
+    public User getUserFromLogin(String login, String password) throws SQLException {
+        Connection connection = connectionPool.checkOut();
+        String sql = "Select * from Account INNER JOIN UserLogin ON Account.UserID = Userlogin.UsID WHERE UserLogin.LoginName = ? AND UserLogin.LoginPassword = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, login);
+        statement.setString(2, password);
+        ResultSet resultSet = statement.executeQuery();
+        User user = null;
+        if (resultSet.next()) {
+            id = resultSet.getInt("UserId");
+            fullName = resultSet.getString("FullName");
+            isAdmin = resultSet.getBoolean("IsAdmin");
+            user = new User(id,fullName,isAdmin);
+        }
+        return user;
+    }
+}
