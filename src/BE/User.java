72841276c1dc74bc1534f618726cc1ae3c7a100a
pete9991 <71692384+pete9991@users.noreply.github.com>
2021-05-04 12:11:14 +0200

    added fill list and getters

--- src/BE/User.java
+++ src/BE/User.java
@@ -31,4 +31,5 @@ public class User {
     public void setAdmin(boolean admin) {
         isAdmin = admin;
     }
+
 }
--- src/BLL/UserManager.java
+++ src/BLL/UserManager.java
@@ -1,4 +1,37 @@
 package BLL;
 
+import BE.User;
+import DAL.DAO.UserDAO;
+
+import java.io.IOException;
+import java.util.List;
+
 public class UserManager {
+    private List<User> allUsers;
+    private List<User> usersList;
+    private List<User> adminsList;
+    public UserManager() throws IOException {
+        this.fillLists();
+    }
+
+    public void fillLists() throws IOException {
+        UserDAO uDAO = new UserDAO();
+        allUsers = uDAO.getAllUsers();
+        for (User u: allUsers) {
+        if(u.isAdmin() == true){
+            adminsList.add(u);
+        }
+        else if(u.isAdmin() == false){
+            usersList.add(u);
+            }
+        }
+    }
+
+    public List<User> getUsersList() {
+        return usersList;
+    }
+
+    public List<User> getAdminsList() {
+        return adminsList;
+    }
 }
