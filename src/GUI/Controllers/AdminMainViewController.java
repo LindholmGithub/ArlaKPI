fda2bdbad57fd1ac854ff2f228dd7be9fec4857a
LindholmGithub <christianlindholm@jubii.dk>
2021-05-04 11:42:36 +0200

    Added GUI to AdminMainView

--- src/GUI/Controllers/AdminMainViewController.java
+++ src/GUI/Controllers/AdminMainViewController.java
@@ -1,4 +1,33 @@
 package GUI.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
 public class AdminMainViewController {
+    @FXML
+    private ListView usersList;
+    @FXML
+    private ListView adminsList;
+    @FXML
+    private Button createButton;
+    @FXML
+    private Button deleteButton;
+    @FXML
+    private Button editInfoButton;
+    @FXML
+    private Button editViewButton;
+
+    public void handleCreateButton(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteButton(ActionEvent actionEvent) {
+    }
+
+    public void handleEditInfoButton(ActionEvent actionEvent) {
+    }
+
+    public void handleEditViewButton(ActionEvent actionEvent) {
+    }
 }
--- src/GUI/Controllers/LoginViewController.java
+++ src/GUI/Controllers/LoginViewController.java
@@ -4,11 +4,19 @@ import BE.User;
 import GUI.Models.LoginModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.SQLException;
 
 public class LoginViewController {
@@ -22,20 +30,32 @@ public class LoginViewController {
     private User selectedUser;
     private LoginModel loginModel;
 
-
     public LoginViewController() throws IOException {
         loginModel = new LoginModel();
     }
 
-    public void handleLoginButton(ActionEvent actionEvent) throws SQLException {
+    public void handleLoginButton(ActionEvent actionEvent) throws SQLException, IOException {
+        Stage thisStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         String writtenLogin = loginField.getText();
         String writtenPassword = passwordField.getText();
         selectedUser = loginModel.getUserFromLogin(writtenLogin,writtenPassword);
         if(selectedUser != null && selectedUser.isAdmin()){
-            System.out.println("Admin!");
-        } else if (selectedUser != null && !selectedUser.isAdmin()) {
-            System.out.println("User!");
-        } else {
+            URL adminUrl = new File("src/GUI/Views/AdminMainView.fxml").toURI().toURL();
+            Parent root = FXMLLoader.load(adminUrl);
+            Scene adminViewScene = new Scene(root,1270,720);
+            thisStage.setScene(adminViewScene);
+            thisStage.hide();
+            thisStage.show();
+        }
+        else if (selectedUser != null && !selectedUser.isAdmin()) {
+            URL userUrl = new File("src/GUI/Views/UserMainView.fxml").toURI().toURL();
+            Parent root = FXMLLoader.load(userUrl);
+            Scene adminViewScene = new Scene(root,1270,720);
+            thisStage.setScene(adminViewScene);
+            thisStage.hide();
+            thisStage.show();
+        }
+        else {
             System.out.println("Failure!");
         }
     }
