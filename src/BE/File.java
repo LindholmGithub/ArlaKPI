06733a2cbfaf4a9118f6bba6f9c84fa7b334286a
WackyGlow <70566908+WackyGlow@users.noreply.github.com>
2021-05-12 16:15:34 +0200

    created File BE for user Views

---
+++ src/BE/File.java
@@ -0,0 +1,38 @@
+package BE;
+
+public class File {
+    private String fileName;
+    private String fileType;
+    private String filePath;
+
+
+    public File(int fileID, String fileName, String fileType, String filePath) {
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.filePath = filePath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
