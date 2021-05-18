f5bcf7ac67ddd61a25c277d7462b6c3341790f2d
LindholmGithub <christianlindholm@jubii.dk>
2021-05-18 10:30:08 +0200

    GemBox is annoying

--- src/GUI/Controllers/UserSelectViewController.java
+++ src/GUI/Controllers/UserSelectViewController.java
@@ -1,7 +1,4 @@
 package GUI.Controllers;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,8 +15,6 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class UserSelectViewController implements Initializable {
